4ef5aa8f706a154fbb37f8092fc00a81af4acd5d
Diego Monteiro <diegomonteiro.dev@gmail.com>
2026-05-15 22:49:29 -0300

    Consulta simples com query methods

--- src/main/java/com/nelioalves/workshopmongo/config/Instantiation.java
+++ src/main/java/com/nelioalves/workshopmongo/config/Instantiation.java
@@ -1,7 +1,6 @@
 package com.nelioalves.workshopmongo.config;
 
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.TimeZone;
 
--- src/main/java/com/nelioalves/workshopmongo/services/UserService.java
+++ src/main/java/com/nelioalves/workshopmongo/services/UserService.java
@@ -50,5 +50,6 @@ public class UserService {
 	public User fromDTO(UserDTO objDto) {
 		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
+	
 
 }
